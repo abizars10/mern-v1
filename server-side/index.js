60bbf24ad315ec0b612215014e216b3d3f7f41ae
Abizar <abizararrifai@gmail.com>
2024-05-04 05:58:18 +0700

    created API

--- server-side/index.js
+++ server-side/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const UserModel = require("./models/Users");
 
 const app = express();
 
-app.get("/", (res, req) => {
-  res.send("Hello World");
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1/crud")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.json(users);
+    console.log("Getting data request!");
+  } catch (err) {
+    console.error("Error getting data", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.listen(3001, () => {
